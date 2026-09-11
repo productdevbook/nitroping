034ddbfacfe1a65857bb25db87f7a84a8d398ab6
productdevbook <hi@productdevbook.com>
2026-09-11 10:20:41 +0200

    feat: enable AGP release Maven publication

--- packages/android/nitroping/build.gradle.kts
+++ packages/android/nitroping/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.plugin.compose")
+    `maven-publish`
 }
 
 group = "dev.nitroping"
@@ -11,6 +12,11 @@ android {
     compileSdk = providers.gradleProperty("nitropingCompileSdk").orElse("37").get().toInt()
 
     buildFeatures { compose = true }
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
 }
 
 dependencies {
@@ -23,4 +29,25 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.activity:activity-compose:1.13.0")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            register<MavenPublication>("release") {
+                from(components["release"])
+                pom {
+                    name.set("NitroPing Android SDK")
+                    description.set("Open-source Android SDK for collecting NitroPing feedback.")
+                    url.set("https://github.com/productdevbook/nitroping")
+                    licenses {
+                        license {
+                            name.set("Apache-2.0")
+                            url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
